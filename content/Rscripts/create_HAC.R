77523405088104c105fe67adaae3534c1174d3af
DanielM-CWF <DanielM@cwf-fcf.org>
2026-01-11 20:10:00 -0800

    Function update

--- content/Rscripts/create_HAC.R
+++ content/Rscripts/create_HAC.R
@@ -1,4 +1,4 @@
-install.packages('jsonlite')
+
 library(jsonlite)
 library(forcats)
 library(dplyr)
@@ -6,20 +6,34 @@ library(ggplot2)
 
 
 
-tracktable<-read.csv("C:/Users/DanielM/Documents/GitHub/bulkley-wcrp-online/content/Rscripts/combined_tracking_table_crossigns_BULK.csv")
-
-tracktable$rank_total_upstr_hab[tracktable$rank_total_upstr_hab == ""] <- NA
-tracktable <- tracktable[!is.na(tracktable$rank_total_upstr_hab), ] #keep only ranked barriers
-
-tracktable<- tracktable %>% arrange(desc(avg_gain_per_barrier)) #sort by avg_gain_per_barrier descending
-
-tracktable <- tracktable %>%
-  mutate(cum_avg_gain = cumsum(avg_gain_per_barrier)) #create cumulative avg_gain_per_barrier column
-
-tracktable_top20<-tracktable %>% slice_max(order_by = avg_gain_per_barrier, n = 20) #set limit to number of structures in HAC (eg n=20)
-
-
-ggplot(tracktable_top20, aes(x = seq_along(cum_avg_gain), y = cum_avg_gain)) +
-  geom_line() +
-  geom_point() +
-  labs(x = "Index", y = "Cumulative Average Gain")
+plot_cumulative_avg_gain <- function(table_path, top_n = 20) {
+  library(dplyr)
+  library(ggplot2)
+  
+  tracktable <- read.csv(table_path)
+  
+  # Convert empty strings to NA and keep only ranked barriers
+  tracktable$rank_total_upstr_hab[tracktable$rank_total_upstr_hab == ""] <- NA
+  tracktable <- tracktable[!is.na(tracktable$rank_total_upstr_hab), ]
+  
+  # Sort by avg_gain_per_barrier (descending)
+  tracktable <- tracktable %>%
+    arrange(desc(avg_gain_per_barrier)) %>%
+    mutate(cum_avg_gain = cumsum(avg_gain_per_barrier))
+  
+  # Take top N barriers
+  tracktable_top <- tracktable %>%
+    slice_max(order_by = avg_gain_per_barrier, n = top_n)
+  
+  # Build and return ggplot
+  ggplot(
+    tracktable_top,
+    aes(x = seq_along(cum_avg_gain), y = cum_avg_gain)
+  ) +
+    geom_line() +
+    geom_point() +
+    labs(
+      x = "Index",
+      y = "Cumulative Average Gain"
+    )
+}
